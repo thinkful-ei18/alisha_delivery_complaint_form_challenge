c83f92c31c5b9ba329bf57a359682fd8fad9064b
alishaevn <alieva.inc@gmail.com>
2018-03-07 13:51:09 -0800

    updated validators and how they will show on screen

--- src/components/TrackingNumber.js
+++ src/components/TrackingNumber.js
@@ -4,9 +4,14 @@ export default class TrackingNumber extends Component {
 
   render() {
 
+    let error;
+    if (this.props.meta.touched && this.props.meta.error) {
+      error = <span className='errorMessage'>{this.props.meta.error}</span>;
+    }
+
     return(
       <div>
-        <label htmlFor='trackingNumber'>Tracking number:</label><br />
+        <label htmlFor='trackingNumber'>Tracking number:{error}</label><br />
         <input
           {...this.props.input}
           id='trackingNumber'
--- src/components/validators.js
+++ src/components/validators.js
@@ -1,8 +1,8 @@
 
-export const required = input => input ? undefined : 'Error: required field';
+export const required = input => input ? undefined : 'Required field';
 
-export const nonEmpty = input => input.trim() !== '' ? undefined : 'Error: cannot be empty';
+export const nonEmpty = input => input.trim() !== '' ? undefined : 'Cannot be empty';
 
-export const exactLength = input => input.length === 5 ? undefined : 'Error: must be 5 characters long';
+export const exactLength = input => input.length === 5 ? undefined : 'Must be 5 characters long';
 
-export const numbersOnly = input => isNaN(input) === false ? undefined : 'Error: numbers only';
+export const numbersOnly = input => isNaN(input) === false ? undefined : 'Numbers only';
