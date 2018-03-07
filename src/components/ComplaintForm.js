28dd6c462c281e456309a6abd5a6fb1207456e0b
alishaevn <alieva.inc@gmail.com>
2018-03-07 15:55:34 -0800

    submit error message shows on screen

--- src/components/ComplaintForm.js
+++ src/components/ComplaintForm.js
@@ -11,7 +11,7 @@ import { required, nonEmpty, exactLength, numbersOnly } from './validators';
 class ComplaintForm extends Component {
 
   onSubmit(values) {
-    console.log(values);
+    // console.log(values);
     return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
       method: 'POST',
       body: JSON.stringify(values),
@@ -20,7 +20,7 @@ class ComplaintForm extends Component {
       }
     })
       .then(res => {
-        console.log('RES: ', res)
+        // console.log('RES: ', res)
         if (!res.ok) {
           if (
             res.headers.has('content-type') &&
@@ -39,9 +39,9 @@ class ComplaintForm extends Component {
         }
         return;
       })
-      .then(() => console.log('Submitted with values', values))
+      // .then(() => console.log('Submitted with values', values))
       .catch(err => {
-        console.log('ERR: ', err);
+        // console.log('ERR: ', err);
         const { reason, message, location } = err;
         if (reason === 'ValidationError') {
           // Convert ValidationErrors into SubmissionErrors for Redux Form
@@ -60,6 +60,23 @@ class ComplaintForm extends Component {
   }
 
   render() {
+    // console.log('this.props: ', this.props);
+    let successMessage;
+    if (this.props.submitSucceeded) {
+      successMessage = (
+        <div className="message message-success">
+          Message submitted successfully
+            </div>
+      );
+    }
+
+    let errorMessage;
+    // console.log('error message inside render:', this.props.submitFailed)
+    if (this.props.submitFailed) {
+      errorMessage = (
+        <div className="message message-error">Submit was unsuccessful</div>
+      );
+    }
 
     return(
       <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
@@ -82,8 +99,10 @@ class ComplaintForm extends Component {
               name='details'
             />
             <br />
-
+          {successMessage}
+          {errorMessage}
             <button type="submit">Submit</button>
+          
             
           </fieldset>
         </form>
